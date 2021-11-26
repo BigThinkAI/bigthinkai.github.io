6756c5d922006eb6ed63a8813c53aee1f770b90a
maybejay <maybejaycodes@hotmail.com>
2021-11-25 22:03:52 -0500

    fix pipeline

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/your-github-repo-name/' : ''
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  swcMinify: false
 }
